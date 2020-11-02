47834bf1ddb2c0d2caeb5620cf4ecb2e3c394ab9
millerbee <millerbgos@gmail.com>
2020-11-02 14:08:52 -0700

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Vegan Power Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Wakanda Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Black Widow Burger", false);
